2be536f957de0338df719815e76132465835d82c
Ege Meriç Erdoğan <egemeric99@gmail.com>
2021-02-14 15:14:00 +0000

    added login screen /accounts/login/

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 from dboperations.operations import db_operations
+from django.http import HttpResponse
 # Create your views here.
 class get_user_apis(View):
     template_name = 'get_user_apis.html'
 
     def get(self, request, *args, **kwargs):
-        db_object = db_operations(self.request)
-        user_apis = db_object.user_apis
-        content={"apis":user_apis}
-        return render(request,self.template_name,content)
+        if request.user.is_authenticated:
+            db_object = db_operations(self.request)
+            user_apis = db_object.user_apis
+            content={"apis":user_apis}
+            return render(request,self.template_name,content)
+        else:
+            return HttpResponse('Unauthorized', status=401)
--- hub/urls.py
+++ hub/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from sensors.views import get_sensor_data
 from accounts.views import get_user_apis
 
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sensors/get_sensor_data/',get_sensor_data.as_view(), name='get_sensor_data'),
     path('accounts/get_user_apis/',get_user_apis.as_view(), name='get_user_apis'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
