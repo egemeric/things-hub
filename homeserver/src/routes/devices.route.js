6e711ca2be5e02e3b9c4926c97a24f43ad18dff5
egemeric <egemeric99@gmail.com>
2021-12-30 21:51:20 +0300

    update api endpoints.

--- homeserver/src/routes/devices.route.js
+++ homeserver/src/routes/devices.route.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const devController = require("../controllers/device.controller")
 const router = express.Router({ mergeParams: true });
-router.route("/devices").get(devController.getDevices)
-router.route("/publish").post(devController.publishMessage)
+router.route("/devices").get(devController.getDevices);
+router.route("/publish").post(devController.publishMessage);
+router.route("/data").get(devController.getDeviceData);
 
 module.exports = router
