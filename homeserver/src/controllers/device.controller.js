ed21ea141dcee9f34b9be59bfc08cc5b791caffd
egemeric <egemeric99@gmail.com>
2021-12-30 21:50:47 +0300

    add device datapoints access.

--- homeserver/src/controllers/device.controller.js
+++ homeserver/src/controllers/device.controller.js
@@ -1,18 +1,49 @@
-const ROOT = "/home/egemeric/"
-const {mqtt_client} = require("../services/mqtt.service")
+const ROOT = "/home/egemeric/";
+const { mqtt_client } = require("../services/mqtt.service");
+const db = require("../models/index");
 
-const getDevices = async (req,res)=>{
-    const devices = Array.from(mqtt.DEVICES)
-    res.send(devices)
-}
+const getDevices = async (req, res) => {
+  const devices = Array.from(mqtt.DEVICES);
+  res.send(devices);
+};
+const getDeviceData = async (req, res) => {
+  const deviceName = req.body?.deviceName || req.query?.deviceName;
+  console.log(deviceName);
+  let device = await db["Device"]
+    .findOne({ where: { deviceName } })
+    .then((result) => {
+      return result;
+    })
+    .catch((e) => {
+      res.status(500).json(e);
+    });
 
-const publishMessage = async (req,res) => {
-    console.log(req.body);
-    mqtt_client.publish(String(req?.body?.topic),String(req?.body?.msg));
-    res.status(200).send();
-}
+  var dataTypes = JSON.parse(device.publishPoints);
+  let filteredData = await db["DeviceData"]
+    .findAll({ where: { ...device?.id } })
+    .then((result) => {
+      let response = {};
+      dataTypes.forEach((element) => {
+        element = element.split("/").pop();
+        let sameDatas = result.filter(obj => obj.dataType === element);
+        response[element] = sameDatas;
+      });
+      return response;
+    })
+    .catch((e) => {
+      res.status(500).json(e);
+    });
+res.status(200).json(filteredData);
+};
+
+const publishMessage = async (req, res) => {
+  console.log(req.body);
+  mqtt_client.publish(String(req?.body?.topic), String(req?.body?.msg));
+  res.status(200).send();
+};
 
 module.exports = {
-    getDevices,
-    publishMessage
-}
+  getDevices,
+  publishMessage,
+  getDeviceData,
+};
