5011162de5a9b3543dba707db534b69d9d5ce213
egemeric <egemeric99@gmail.com>
2022-01-08 18:11:27 +0300

    added mqtt image publisher

--- homeserver/imagePublisher/main.py
+++ homeserver/imagePublisher/main.py
@@ -1,10 +1,27 @@
+import base64
+import time
+import io
+import datetime as dt
+import picamera 
 import paho.mqtt.publish as publish
-MQTT_SERVER = "localhost"  #Write Server IP Address
-MQTT_PATH = "/image"
+MQTT_SERVER = "home.egemeric.gen.tr"  #Write Server IP Address
+MQTT_PATH = "/home/egemeric/camera"
 
-f=open("test.jpg", "rb") #3.7kiB in same folder
-fileContent = f.read()
-byteArr = bytearray(fileContent)
+camera = picamera.PiCamera()
+camera.start_preview()
+time.sleep(1)
+def camJob():
+    my_stream = io.BytesIO()
+    camera.resolution = (1024, 768)
+    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    camera.capture(my_stream, 'jpeg', quality=10)
+    print(my_stream.getbuffer().nbytes)
+    my_stream.seek(0)
+    publish.single(MQTT_PATH, my_stream.read(), hostname=MQTT_SERVER)
 
+while(True):
+    camJob()
+    time.sleep(0.5)
 
-publish.single(MQTT_PATH, byteArr, hostname=MQTT_SERVER)
+camera.stop_preview()
+camera.stop()
